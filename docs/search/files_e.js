0fa90e57ee8c73e6132b0604c6c021782f1c8ecc
Michel Megens <dev@bietje.net>
2016-11-01 07:42:45 +0100

    docs: update documentation to ETA/OS 4.6.0
    Signed-off-by: Michel Megens <dev@bietje.net>

---
+++ docs/search/files_e.js
@@ -0,0 +1,14 @@
+var searchData=
+[
+  ['sched_2eh',['sched.h',['../include_2etaos_2sched_8h.html',1,'']]],
+  ['semaphore_2eh',['semaphore.h',['../semaphore_8h.html',1,'']]],
+  ['spi_2dcore_2ec',['spi-core.c',['../spi-core_8c.html',1,'']]],
+  ['spi_2eh',['spi.h',['../spi_8h.html',1,'']]],
+  ['spinlock_2eh',['spinlock.h',['../arch_2avr_2include_2asm_2spinlock_8h.html',1,'']]],
+  ['spinlock_2eh',['spinlock.h',['../include_2etaos_2spinlock_8h.html',1,'']]],
+  ['sram_2eh',['sram.h',['../sram_8h.html',1,'']]],
+  ['strchr_2ec',['strchr.c',['../strchr_8c.html',1,'']]],
+  ['strcmp_2ec',['strcmp.c',['../strcmp_8c.html',1,'']]],
+  ['strlen_2ec',['strlen.c',['../strlen_8c.html',1,'']]],
+  ['systick_2ec',['systick.c',['../systick_8c.html',1,'']]]
+];
---
+++ docs/search/functions_f.js
@@ -0,0 +1,6 @@
+var searchData=
+[
+  ['queue_5fadd_5fthread',['queue_add_thread',['../group__sched.html#ga72ca187dd5dd0ac19af98da3ea9b56e4',1,'core.c']]],
+  ['queue_5fremove_5fthread',['queue_remove_thread',['../group__sched.html#gaa468a0cc0864610b821b2730abe5c182',1,'core.c']]],
+  ['queue_5fwait_5ftmo',['queue_wait_tmo',['../group__sched.html#gabb6df12a0f0c3721e58b5250052a42a2',1,'core.c']]]
+];
